1dc29241ee16e4e583d3bdf752a5d30b5065be8a
Matthew Toon [sc22mt] <sc22mt@comp-pc6217.leeds.ac.uk>
2023-12-08 13:27:18 +0000

    1st profile page iteration

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -56,14 +56,14 @@ void MainWindow::pauseVideo() {
         mediaPlayer->play();
     }
 }
-// void MainWindow::showVideo() {
+void MainWindow::showVideo() {
 
 //     // Set the media content to the provided video file path
 //     mediaPlayer->setMedia(QUrl::fromLocalFile("videos/c.mp4"));
 
 //     // Play the video
 //     mediaPlayer->play();
-// }
+}
 
 
 void MainWindow::createMainContent() {
@@ -73,7 +73,7 @@ void MainWindow::createMainContent() {
 
     QVBoxLayout *layout = new QVBoxLayout(centralWidget);
 
-    QLabel *contentLabel = new QLabel("Main Co/*ntent Goes Here");
+    QLabel *contentLabel = new QLabel("Main Content Goes Here");
     contentLabel->setAlignment(Qt::AlignCenter);
     layout->addWidget(contentLabel);
 }
@@ -95,6 +95,7 @@ void MainWindow::showAddVideos() {
 }
 
 void MainWindow::showFeed() {
+
     // Implement the logic to show the "Feed" section
     // Create a list widget for displaying videos in the feed
     QListWidget *feedListWidget = new QListWidget(this);
@@ -107,47 +108,47 @@ void MainWindow::showFeed() {
     // Populate the feed list with videos and sharers
     for (int i = 0; i < videoTitles.size(); ++i) {
 
-    // Create a list item
-    QListWidgetItem *item = new QListWidgetItem(feedListWidget);
-    item->setSizeHint(QSize(250, 150));  // Set the size of each item
+        // Create a list item
+        QListWidgetItem *item = new QListWidgetItem(feedListWidget);
+        item->setSizeHint(QSize(250, 150));  // Set the size of each item
 
-     // Create a custom widget for the list item
-     QWidget *videoItemWidget = new QWidget(feedListWidget);
-     videoItemWidget->setStyleSheet("background-color: #f5f5f5; border: 1px solid #ccc; border-radius: 8px; margin: 5px;");
+         // Create a custom widget for the list item
+        QWidget *videoItemWidget = new QWidget(feedListWidget);
+        videoItemWidget->setStyleSheet("background-color: #f5f5f5; border: 1px solid #ccc; border-radius: 8px; margin: 5px;");
 
-     // Create a layout for the widget
-     QVBoxLayout *itemLayout = new QVBoxLayout(videoItemWidget);
+         // Create a layout for the widget
+         QVBoxLayout *itemLayout = new QVBoxLayout(videoItemWidget);
 
-     // Add video thumbnail (replace "path_to_thumbnail" with the actual path or URL)
-     QLabel *thumbnailLabel = new QLabel(videoItemWidget);
-    thumbnailLabel->setPixmap(QPixmap("path_to_thumbnail").scaled(150, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    thumbnailLabel->setAlignment(Qt::AlignCenter);
-    itemLayout->addWidget(thumbnailLabel);
+         // Add video thumbnail (replace "path_to_thumbnail" with the actual path or URL)
+         QLabel *thumbnailLabel = new QLabel(videoItemWidget);
+         thumbnailLabel->setPixmap(QPixmap("path_to_thumbnail").scaled(150, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+         thumbnailLabel->setAlignment(Qt::AlignCenter);
+         itemLayout->addWidget(thumbnailLabel);
 
-    // Add video title label
-    QLabel *titleLabel = new QLabel(videoTitles.at(i), videoItemWidget);
-    titleLabel->setAlignment(Qt::AlignCenter);
-    titleLabel->setStyleSheet("font-weight: bold; margin-top: 5px;");
-    itemLayout->addWidget(titleLabel);
+        // Add video title label
+        QLabel *titleLabel = new QLabel(videoTitles.at(i), videoItemWidget);
+        titleLabel->setAlignment(Qt::AlignCenter);
+        titleLabel->setStyleSheet("font-weight: bold; margin-top: 5px;");
+         itemLayout->addWidget(titleLabel);
 
-    // Add sharer label
-    QLabel *sharerLabel = new QLabel("Shared by: " + sharers.at(i), videoItemWidget);
-    sharerLabel->setAlignment(Qt::AlignCenter);
-    sharerLabel->setStyleSheet("font-style: italic; color: #888;");
-    itemLayout->addWidget(sharerLabel);
+        // Add sharer label
+        QLabel *sharerLabel = new QLabel("Shared by: " + sharers.at(i), videoItemWidget);
+        sharerLabel->setAlignment(Qt::AlignCenter);
+        sharerLabel->setStyleSheet("font-style: italic; color: #888;");
+        itemLayout->addWidget(sharerLabel);
 
-    // Set the widget for the list item
-    feedListWidget->setItemWidget(item, videoItemWidget);
+        // Set the widget for the list item
+        feedListWidget->setItemWidget(item, videoItemWidget);
 
         }
 
-        // Connect the list item click event to a slot (you can implement this slot)
-        connect(feedListWidget, &QListWidget::itemClicked, this, [this](){ Q_UNUSED(this); });
+    // Connect the list item click event to a slot (unimplemented)
+    connect(feedListWidget, &QListWidget::itemClicked, this, [this](){ Q_UNUSED(this); });
 
     qDebug("Showing Feed section");
 
-
 }
+
 void MainWindow::AttemptLogin(){
     qDebug("Processing Login");
 
@@ -178,82 +179,118 @@ void MainWindow::ShowLogin(){
 }
 
 void MainWindow::showProfile() {
-    // Check if the profile widget is already created
-    if (!profileWidget) {
-        // 1st Layer: Top left - Button to go to settings
-        QPushButton *settingsButton = new QPushButton("Settings", this);
-        // Connect the button to a lambda function that does nothing for testing purposes
-        connect(settingsButton, &QPushButton::clicked, [this](){ Q_UNUSED(this); });
 
-        // 1st Layer: Top center - Large text "Profile"
-        QLabel *profileLabel = new QLabel("Profile", this);
-        profileLabel->setAlignment(Qt::AlignCenter);
+    // 1st Layer: Top left - Button to go to settings
+    QPushButton *settingsButton = new QPushButton("Settings", this);
+    connect(settingsButton, &QPushButton::clicked, [this](){ Q_UNUSED(this); });
 
-        // 1st Layer: Top right - Button to log out
-        QPushButton *logoutButton = new QPushButton("Logout", this);
-        // Connect the button to a lambda function that does nothing for testing purposes
-        connect(logoutButton, &QPushButton::clicked, [this](){ Q_UNUSED(this); });
+    // 1st Layer: Top center - Large text "Profile"
+    QLabel *profileLabel = new QLabel("Your Profile", this);
+    profileLabel->setStyleSheet("font-size: 16pt;");
+    profileLabel->setAlignment(Qt::AlignCenter);
 
-        // Create a horizontal layout for the 1st layer
-        QHBoxLayout *layer1Layout = new QHBoxLayout();
+    // 1st Layer: Top right - Button to log out
+    QPushButton *logoutButton = new QPushButton("Logout", this);
+    connect(logoutButton, &QPushButton::clicked, [this](){ Q_UNUSED(this); });
 
-        // Add the 1st layer elements to the layout
-        layer1Layout->addWidget(settingsButton);
-        layer1Layout->addWidget(profileLabel);
-        layer1Layout->addWidget(logoutButton);
-        // 2nd Layer: Center - Circular space for profile image
-        QHBoxLayout *profileImageLayout = new QHBoxLayout();
+    // Create a horizontal layout for the 1st layer
+    QHBoxLayout *layer1Layout = new QHBoxLayout();
+    layer1Layout->addWidget(settingsButton);
+    layer1Layout->addWidget(profileLabel);
+    layer1Layout->addWidget(logoutButton);
 
-        QLabel *profileImageLabel = new QLabel(this);
-        // Set a circular border for the profile image
-        profileImageLabel->setStyleSheet("border-radius: 50%; border: 2px solid #a0a0a0;");
-        // Set the profile image (replace "path_to_profile_image" with the actual path or URL)
-        profileImageLabel->setFixedSize(100, 100); // Set the size of the circular space
-        profileImageLabel->setPixmap(QPixmap("/home/csunix/sc22mt/Pictures/bombadil.jpg")); // Image doesn't seem to take the border radius settings
+    // 2nd Layer: Rounded space for profile image
+    QLabel *profileImageLabel = new QLabel(this);
+    profileImageLabel->setStyleSheet("border-radius: 50%; border: 2px solid #a0a0a0;");
+    profileImageLabel->setFixedSize(150, 150);
+    //profileImageLabel->setPixmap(QPixmap("/home/csunix/sc22mt/Pictures/bombadil.jpg"));
 
-        profileImageLayout->addWidget(profileImageLabel); // Added to avoid memory leak issues
+    // Create a vertical layout for the 2nd layer
+    QHBoxLayout *layer2Layout = new QHBoxLayout();
+    layer2Layout->addWidget(profileImageLabel, 0, Qt::AlignHCenter);
+
+    // 3rd Layer: Username
+    QLabel *usernameLabel = new QLabel("Username", this);
+    usernameLabel->setAlignment(Qt::AlignCenter);
+    usernameLabel->setStyleSheet("font-size: 28pt;");
+
+    // Create a vertical layout for the 3rd layer
+    QVBoxLayout *layer3Layout = new QVBoxLayout();
+    layer3Layout->addWidget(usernameLabel);
+
+    // 4th Layer: Center - User description
+    QLabel *descriptionLabel = new QLabel("User Description", this);
+    descriptionLabel->setAlignment(Qt::AlignCenter);
+
+    // Create a vertical layout for the 4th layer
+    QVBoxLayout *layer4Layout = new QVBoxLayout();
+    layer4Layout->addWidget(descriptionLabel);
+
+    // 5th Layer: Custom widget for each post
+    class PostWidget : public QWidget {
+    public:
+        PostWidget(const QString& postText, const QString& profileImagePath, const QString& posterName, QWidget* parent = nullptr) : QWidget(parent) {
+
+            QLabel* profileImageLabel = new QLabel(this);
+            profileImageLabel->setFixedSize(50, 50);
+            profileImageLabel->setPixmap(QPixmap(profileImagePath));
+            profileImageLabel->setStyleSheet("border-radius: 50%; border: 2px solid #a0a0a0;");
+
+            QLabel* nameLabel = new QLabel(posterName, this);
+            nameLabel->setStyleSheet("font-weight: bold;");
+
+            QLabel* postTextLabel = new QLabel(postText, this);
+            postTextLabel->setWordWrap(true);
+
+            QHBoxLayout* postLayout = new QHBoxLayout(this);
+            postLayout->addWidget(profileImageLabel);
+            postLayout->addSpacing(10);
+            QVBoxLayout* textLayout = new QVBoxLayout();
+            textLayout->addWidget(nameLabel, 0, Qt::AlignLeft);
+            textLayout->addWidget(postTextLabel);
+            postLayout->addLayout(textLayout);
+
+            postLayout->setSpacing(10);
+            postLayout->setContentsMargins(10, 10, 10, 10);
+        }
+    };
+
+    // 5th Layer: - Create 3 PostWidgets,
+    PostWidget* post1Widget = new PostWidget("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas... ", "/home/csunix/sc22mt/Pictures/bombadil.jpg", "Title 1", this);
+    PostWidget* post2Widget = new PostWidget("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas...", "/home/csunix/sc22mt/Pictures/bombadil.jpg", "Title 2", this);
+    PostWidget* post3Widget = new PostWidget("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas...", "/home/csunix/sc22mt/Pictures/bombadil.jpg", "Title 3", this);
+
+    // Create a vertical layout for the 5th layer
+    QVBoxLayout* layer5Layout = new QVBoxLayout();
+    layer5Layout->addWidget(post1Widget);
+    layer5Layout->addWidget(post2Widget);
+    layer5Layout->addWidget(post3Widget);
+
+    // Set spacing for the 5th layer
+    layer5Layout->setSpacing(10);
+
+    // Create the profile widget
+    profileWidget = new QWidget(this);
+
+    // Set the layout for the profile widget as a vertical layout
+    QVBoxLayout *profileLayout = new QVBoxLayout(profileWidget);
+    profileLayout->addLayout(layer1Layout);
+    profileLayout->addStretch(1);
+    profileLayout->addLayout(layer2Layout);
+    profileLayout->addLayout(layer3Layout);
+    profileLayout->addLayout(layer4Layout);
+    profileLayout->addStretch(2);
+    profileLayout->addLayout(layer5Layout);
+    layer5Layout->setAlignment(Qt::AlignHCenter);
 
 
-        // Add the profile image label to the layout
-        //profileImageLayout->addWidget(profileImageLabel, 0, Qt::AlignCenter);
-
-
-        // 3rd Layer: Center - Username
-        QLabel *usernameLabel = new QLabel("Username", this);
-        usernameLabel->setAlignment(Qt::AlignCenter);
-
-        // 4th Layer: Center - User description
-        QLabel *descriptionLabel = new QLabel("User Description", this);
-        descriptionLabel->setAlignment(Qt::AlignCenter);
-
-        // 5th Layer: Center - 3 vertical timestamped text boxes
-        // You can customize the content and appearance as needed
-        QLabel *post1 = new QLabel("Post 1 - Timestamp", this);
-        QLabel *post2 = new QLabel("Post 2 - Timestamp", this);
-        QLabel *post3 = new QLabel("Post 3 - Timestamp", this);
-
-        // Create the profile widget
-        profileWidget = new QWidget(this);
-
-        // Set the layout for the profile widget as a vertical layout
-        QVBoxLayout *profileLayout = new QVBoxLayout(profileWidget);
-        profileLayout->addLayout(layer1Layout);  // Add the 1st layer layout
-        profileLayout->addWidget(profileImageLabel, 0, Qt::AlignHCenter);
-        profileLayout->addWidget(usernameLabel);
-        profileLayout->addWidget(descriptionLabel);
-        profileLayout->addWidget(post1);
-        profileLayout->addWidget(post2);
-        profileLayout->addWidget(post3);
-
-        // Ensure the navigation toolbar is visible at the bottom
-        navToolBar->setVisible(true);
-    }
+    // Ensure the navigation toolbar is visible at the bottom
+    navToolBar->setVisible(true);
 
     // Set the profile widget as the central widget
     setCentralWidget(profileWidget);
 
     qDebug("Showing Profile section");
-
 }
 
 void MainWindow::showOwnVideos() {
